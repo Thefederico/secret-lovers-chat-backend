77e1c9384a719d29261846ecf45f16b3f8d7b2e7
Federico Upegui Restrepo <federico_upegui.dev@outlook.com>
2022-07-10 19:13:12 -0500

    Server

---
+++ main.js
@@ -0,0 +1,28 @@
+const server = require('http').createServer()
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*'
+  }
+})
+
+const PORT = 3001
+const NEW_CHAT_MESSAGE = 'newMessage'
+
+io.on('connection', socket => {
+  console.log(`Client ${socket.id} connected`)
+
+  const { chadId } = socket.handshake.query
+  socket.join(chadId)
+
+  socket.on(NEW_CHAT_MESSAGE, data => {
+    io.in(chadId).emit(NEW_CHAT_MESSAGE, data)
+  })
+
+  socket.on('disconnect', () => {
+    socket.leave(chadId)
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server on port: ${PORT}`)
+})
